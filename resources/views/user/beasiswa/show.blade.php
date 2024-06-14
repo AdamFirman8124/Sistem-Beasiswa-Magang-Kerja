4483682ea1046f1f57a591ae29c08368b60d87a5
AdamFirman8124 <adamfirman078@gmail.com>
2024-06-14 13:43:11 +0700

    Update

--- resources/views/user/beasiswa/show.blade.php
+++ resources/views/user/beasiswa/show.blade.php
@@ -26,13 +26,6 @@
                         <span class="fw-bold">Persyaratan</span>
                         <textarea class="form-control text-area-detail mb-3" id="exampleFormControlTextarea1" rows="6" readonly>{{ $beasiswa->requirement }}</textarea>
                         <hr class="line-hr">
-                        <div class="d-flex justify-content-end">
-                            @if(!$isRegistered)
-                                <button type="button" class="btn btn-outline-dark btn-sm me-2" onclick="confirmBeasiswa('{{ $beasiswa->id }}')">Daftar Beasiswa</button>
-                            @else
-                                <button type="button" class="btn btn-outline-dark btn-sm me-2" disabled>Sudah Terdaftar</button>
-                            @endif
-                        </div>
                     </div>
                 </div>
             </div>
@@ -40,90 +33,5 @@
         <hr class="line-hr">
     </div>
 @endsection
-
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script>
-function confirmBeasiswa(id) {
-    const randomSerial = 'B' + Math.random().toString().substr(2, 8); // Generates random 8 digits
-
-    Swal.fire({
-        title: 'Apakah Anda menerima beasiswa ini?',
-        html: `
-        <p>Silahkan masukkan bukti penerimaan beasiswa</p>
-            <input id="nama" class="swal2-input" name="nama" value="{{ session('name') }}" hidden>
-            <input id="beasiswa_id" type="text" value="${id}" name="beasiswa_id" hidden>
-            <input id="serial" class="swal2-input" name="serial" value="${randomSerial}" hidden readonly>
-            <input type="file" id="bukti" class="form-control" name="bukti" placeholder="Bukti penerimaan">
-        `,
-        icon: 'question',
-        showCancelButton: true,
-        confirmButtonText: 'Ya, Saya Terima',
-        cancelButtonText: 'Batal',
-        focusConfirm: false,
-        preConfirm: () => {
-            const nama = Swal.getPopup().querySelector('#nama').value;
-            const beasiswa_id = Swal.getPopup().querySelector('#beasiswa_id').value;
-            const serial = Swal.getPopup().querySelector('#serial').value;
-            const bukti = Swal.getPopup().querySelector('#bukti').files[0];
-
-            const formData = new FormData();
-            formData.append('nama', nama);
-            formData.append('beasiswa_id', beasiswa_id);
-            formData.append('serial', serial);
-            formData.append('bukti', bukti);
-            formData.append('_token', '{{ csrf_token() }}');
-
-            return fetch(`{{ route('user.beasiswa.update') }}`, {
-                method: 'POST',
-                body: formData
-            }).then(response => {
-                if (!response.ok) {
-                    throw new Error(response.statusText);
-                }
-                return response.json();
-            }).then(data => {
-                console.log(data); // Tambahkan ini untuk debugging
-                Swal.fire('Berhasil!', 'Data berhasil disimpan. Berikut adalah serial number Anda: <strong>' + data.serial + '</strong>. Jangan dihilangkan, kalau bisa di screenshot.', 'success').then(() => {
-                    location.reload();
-                });
-            }).catch(error => {
-                Swal.showValidationMessage(`Request failed: ${error.message}`);
-            });
-        }
-    }).then((result) => {
-        if (result.isConfirmed) {
-            const formData = new FormData();
-            formData.append('_token', '{{ csrf_token() }}');
-            formData.append('nama', result.value.name);
-            formData.append('beasiswa_id', result.value.beasiswa_id);
-            formData.append('serial', result.value.serial);
-            formData.append('bukti', result.value.bukti);
-
-            $.ajax({
-                type: 'POST',
-                url: '{{ route('user.beasiswa.update') }}',
-                data: formData,
-                processData: false,
-                contentType: false,
-                success: function (response) {
-                    Swal.fire({
-                        title: 'Berhasil!',
-                        html: 'Data berhasil disimpan. Berikut adalah kode referal Anda: <strong>' + response.serial + '</strong>. Jangan dihilangkan, kalau bisa di screenshot.',
-                        icon: 'success'
-                    }).then(() => {
-                        location.reload();
-                    });
-                },
-                error: function (xhr) {
-                    Swal.fire(
-                        'Error!',
-                        xhr.responseJSON.error || 'Terjadi kesalahan saat menambahkan data.',
-                        'error'
-                    );
-                }
-            });
-        }
-    });
-}
 </script>
 
--- resources/views/user/loker/show.blade.php
+++ resources/views/user/loker/show.blade.php
@@ -25,86 +25,10 @@
                         <hr class="line-hr">
                         <span class="fw-bold">Persyaratan</span>
                         <textarea class="form-control text-area-detail mb-3" id="exampleFormControlTextarea1" rows="6" readonly>{{ $loker->requirement }}</textarea>
-                        <hr class="line-hr">
-                        <div class="d-flex justify-content-end">
-                            @if(\App\Models\MagangUser::where('nama', session('name'))->where('magang_id', $loker->id)->exists())
-                                <button type="button" class="btn btn-outline-dark btn-sm me-2" disabled>Sudah Daftar</button>
-                            @else
-                                <button type="button" class="btn btn-outline-dark btn-sm me-2" onclick="addMagangUser('{{ $loker->id }}',  '{{ session('name') }}')">Daftar Kerja</button>
-                            @endif
-                        </div>
+                        <hr class="line-hr">                        
                     </div>
                 </div>
             </div>
         </div>
-        <hr class="line-hr">
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-
-    <script>
-        function addMagangUser(id, name) {
-            const randomSerial = 'M' + Math.random().toString().substr(2, 8); // Generates random 8 digits
-
-            Swal.fire({
-                title: 'Apakah anda pernah magang di perusahaan ini?',
-                html: `
-                    <p>Silahkan masukkan Durasi dan bukti keikutsertaan</p>
-                    <input id="nama" class="swal2-input" name="nama" value="${name}" hidden>
-                    <input id="id_loker" type="text" value="${id}" name="magang_id" hidden>
-                    <input id="durasi" class="swal2-input" name="durasi" placeholder="Durasi">
-                    <input id="serial" class="swal2-input" name="serial" value="${randomSerial}" hidden readonly>
-                    <input type="file" id="bukti" class="form-control" name="bukti" placeholder="Bukti penerimaan">
-                `,
-                focusConfirm: false,
-                preConfirm: () => {
-                    const nama = Swal.getPopup().querySelector('#nama').value;
-                    const durasi = Swal.getPopup().querySelector('#durasi').value;
-                    const magang_id = Swal.getPopup().querySelector('#id_loker').value;
-                    const serial = Swal.getPopup().querySelector('#serial').value;
-                    const bukti = Swal.getPopup().querySelector('#bukti').files[0];
-
-                    if (!durasi) {
-                        Swal.showValidationMessage('Durasi harus diisi');
-                        return false;
-                    }
-                    return { nama: nama, durasi: durasi, magang_id: magang_id, serial: serial, bukti: bukti };
-                }
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    const formData = new FormData();
-                    formData.append('_token', '{{ csrf_token() }}');
-                    formData.append('nama', result.value.nama);
-                    formData.append('durasi', result.value.durasi);
-                    formData.append('magang_id', result.value.magang_id);
-                    formData.append('serial', result.value.serial);
-                    formData.append('bukti', result.value.bukti);
-
-                    $.ajax({
-                        type: 'POST',
-                        url: '{{ route('user.loker.update') }}',
-                        data: formData,
-                        processData: false,
-                        contentType: false,
-                        success: function (response) {
-                            Swal.fire({
-                                title: 'Berhasil!',
-                                html: 'Data berhasil disimpan. Berikut adalah kode referal Anda: <strong>' + result.value.serial + '</strong>. Jangan dihilangkan, kalau bisa di screenshot.',
-                                icon: 'success'
-                            }).then(() => {
-                                location.reload();
-                            });
-                        },
-                        error: function (xhr) {
-                            Swal.fire(
-                                'Error!',
-                                xhr.responseJSON.error || 'Terjadi kesalahan saat menambahkan data.',
-                                'error'
-                            );
-                        }
-                    });
-                }
-            });
-        }
-    </script>
 @endsection
